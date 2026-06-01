20de2ae1e415c06c8a8e63be000c4fe1b51b30e9
Bhuvanesh <bhuvaneshgadi24@gmail.com>
2026-06-01 07:08:41 +0530

    written some allowed hosts

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-ahr!9hl%5vlc%$5+hypzu^v84_i33xr&30)!&29uc$ifncomsu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'dhanvantri-backend-3par.onrender.com']
 
 
 # Application definition
